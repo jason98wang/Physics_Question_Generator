8d943417bd14c2bbc4d041c5800ccc44c77223f0
veggieman420 <joshdoesstuff4fun@gmail.com>
2018-11-20 10:58:14 -0500

    UI init

---
+++ src/Exe.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class Exe {
+
+	private static JFrame window;
+	
+	public static void main(String[] args) {
+		
+		window = new QuizTaker();
+
+	}
+
+}
---
+++ src/QuizTaker.java
@@ -0,0 +1,21 @@
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class QuizTaker extends JFrame{
+	
+	QuizTaker(){
+		super("Practice Like A Physicist");
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+		this.setResizable(false);
+		this.requestFocusInWindow();
+		this.setVisible(true);
+	}
+	
+	private class mainPanel extends JPanel{
+		
+	}
+}
