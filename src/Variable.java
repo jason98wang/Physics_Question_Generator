5b1188257076dacdbdced4ac39a5b5ce0a24e983
bl21812 <39709270+bl21812@users.noreply.github.com>
2018-11-28 10:29:46 -0500

    Added g as a constant

--- src/Variable.java
+++ src/Variable.java
@@ -17,7 +17,9 @@ public class Variable extends Symbol{
 		}
         if (id.equals("pi")) {
             value = Math.PI;
-        } else {
+        } else if (id.equals("g")) {
+		value = 9.81;
+	} else {
 	    	try {
 	    	    value = Double.parseDouble(id);
             } catch (NumberFormatException e) {
