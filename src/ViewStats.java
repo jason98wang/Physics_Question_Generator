530084a1036fa358a8a29ec8447e534de45dcd9d
jason98wang <38818672+jason98wang@users.noreply.github.com>
2018-12-09 11:48:42 -0500

    asd

--- src/ViewStats.java
+++ src/ViewStats.java
@@ -19,7 +19,12 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
-
+/*
+ * [ViewStats.java]
+ * The page where students can check out their life time account stats
+ * Author: Jason Wang
+ * Nov. 20, 2018
+ */
 public class ViewStats extends JFrame {
 	private static JFrame window;
 
@@ -39,6 +44,7 @@ public class ViewStats extends JFrame {
 	// Constructor
 	ViewStats(Student student) {
 		super("Summary Page");
+		
 		// Set the frame to full screen
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(Toolkit.getDefaultToolkit().getScreenSize());
@@ -47,7 +53,7 @@ public class ViewStats extends JFrame {
 		// Set up the main panel
 		JPanel panel = new JPanel();
 		panel.setPreferredSize(Toolkit.getDefaultToolkit().getScreenSize());
-//		panel.setMinimumSize(panel.getPreferredSize());
+		panel.setMinimumSize(panel.getPreferredSize());
 		panel.setSize(panel.getPreferredSize());
 		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 		this.setUndecorated(true);
@@ -57,10 +63,8 @@ public class ViewStats extends JFrame {
 
 		// Make the frame visible
 		this.setVisible(true);
-
 		this.setContentPane(panel);
-
-
+	
 		this.student = student;
 		studentName = student.getName();
 		studentWrongQuestions = student.getIncorrectQuestions();
@@ -68,6 +72,7 @@ public class ViewStats extends JFrame {
 		accuracy = ((studentTotalQuestions - studentWrongQuestions) / (double) studentTotalQuestions) * 100.00;
 
 
+		//creating the back button the leave the page
 		back = new JButton(new ImageIcon("BackButton.png"));
 		back.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -78,37 +83,45 @@ public class ViewStats extends JFrame {
 		back.setContentAreaFilled(false); 
 		back.setBorder(BorderFactory.createEmptyBorder());
 
+		//creating the label with the student's name
 		JLabel name = new JLabel(studentName);
 		name.setFont(new Font("Serif", Font.BOLD, 100));
 		name.setForeground(orange);
 		name.setSize(name.getPreferredSize());
 		name.setAlignmentX(JLabel.CENTER_ALIGNMENT);
 		
+		//adding the label to the main panel and changing the background color
 		panel.setBackground(indigo);
 		panel.add(name);
 
+		//creating the life time accuracy label
 		JLabel stats = new JLabel(String.format("Accuracy " + "%.2f", accuracy) + "%");
 		stats.setFont(new Font("Serif", Font.BOLD, 150));
 		stats.setAlignmentX(JTextArea.CENTER_ALIGNMENT);
 		stats.setHorizontalAlignment(JLabel.CENTER);
 		stats.setForeground(lightBlue);
 
+		//creating the life time total incorrect questions panel
 		JLabel wrong = new JLabel("Total Incorrect: " + Integer.toString(studentWrongQuestions));
 		wrong.setFont(new Font("Serif", Font.BOLD, 50));
 		wrong.setHorizontalAlignment(JLabel.CENTER);
 		wrong.setForeground(lightBlue);
 
+		//creating the life time total questions panel
 		JLabel total = new JLabel("Total Questions: " + Integer.toString(studentTotalQuestions));
 		total.setFont(new Font("Serif", Font.BOLD, 50));
 		total.setHorizontalAlignment(JLabel.CENTER);
 		total.setForeground(lightBlue);
 
+		//adding extra spacce between the panels
 		panel.add(Box.createRigidArea(new Dimension(0, 100)));
 		
+		//adding the accuracy to the main panel
 		panel.add(stats);
 		
 		panel.add(Box.createRigidArea(new Dimension(0, 150)));
 		
+		//adding labels to the left side of the screen
 		JPanel panel1 = new JPanel();
 		panel1.add(wrong);
 		panel1.add(Box.createRigidArea(new Dimension(100, 0)));
@@ -116,14 +129,15 @@ public class ViewStats extends JFrame {
 		panel1.setLayout(new BoxLayout(panel1, BoxLayout.X_AXIS));
 		panel1.setBackground(indigo);
 		panel.add(panel1);
-		
-//		panel.add(Box.createRigidArea(new Dimension(0, 150)));
-		back.setAlignmentX(JButton.LEFT_ALIGNMENT);
-		JPanel a = new JPanel();
-		a.add(back);
-		a.setLayout(new FlowLayout(FlowLayout.LEFT));
-		a.setBackground(indigo);
-		panel.add(a);
+		panel.add(Box.createRigidArea(new Dimension(0,150)));
+			
+		//creating a panel to add the backButton and align it to the left side
+		JPanel backPanel = new JPanel();
+		backPanel.add(Box.createRigidArea(new Dimension(100,0)));
+		backPanel.add(back);
+		backPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		backPanel.setBackground(indigo);
+		panel.add(backPanel);
 		
 		
 	
