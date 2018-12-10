3c7f4863bbdff2ec54ddfc3dcd1137ebe9a841e8
bl21812 <39709270+bl21812@users.noreply.github.com>
2018-12-09 21:43:59 -0500

    Commented Unit

--- src/Unit.java
+++ src/Unit.java
@@ -1,3 +1,11 @@
+/**
+ * [Unit.java]
+ * Object representing a unit in a course
+ * Authors: Jason Wang, Yili Liu, Eric Long, Josh Cai, Brian Li
+ * December 09, 2018
+ */
+
+//Data structure imports
 import data_structures.SimpleLinkedList;
 
 public class Unit {
@@ -9,52 +17,93 @@ public class Unit {
 	private SimpleLinkedList<Question> incorrectQuestions;
 	private int questionsTaken;
 
-	// constructor
+	/**
+	 * Unit
+	 * Constructor that makes a unit with a given name and number
+	 * @param name, the name of the unit
+	 * @param num, the unit number
+	 */
 	Unit(String name, int num){
 		this.name = name;
 		this.num = num;
 		questions = new SimpleLinkedList<Question>();
 		incorrectQuestions = new SimpleLinkedList<Question>();
-	}
+	} //End of constructor
 
+	/**
+	 * answeredQuestion
+	 * Updates question counts and incorrect question list
+	 * @param q, the Question answered
+	 * @param correct, whether the question was answered correctly (boolean)
+	 */
 	public void answeredQuestion(Question q, boolean correct) {
 		questionsTaken++;
 		if (!correct) {
 			incorrectQuestions.add(q);
 		}
-	}
+	} //End of answeredQuestion
 
+	/**
+	 * addQuestion
+	 * Adds a question to the unit
+	 * @param q, the question object to be added
+	 */
 	public void addQuestion(Question q) {
 		questions.add(q);
-	}
+	} //End of addQuestion
 
+	/**
+	 * removeQuestion
+	 * Removes a given question from the unit's list of questions
+	 * @return q, the question object to be removed
+	 */
 	public void removeQuestion(Question q) {
 		questions.remove(q);
-	}
+	} //End of removeQuestion
 
-	// getters
+	/**
+	 * getName
+	 * Returns the unit name
+	 * @return name, the unit name (String)
+	 */
 	public String getName() {
 		return name;
-	}
+	} //End of getName
 
+	/**
+	 * getNum
+	 * Returns the unit number
+	 * @return num, the unit number (integer)
+	 */
 	public int getNum() {
 		return num;
-	}
+	} //End of getNum
 
+	/**
+	 * getQuestions
+	 * Returns the questions in the unit
+	 * @return questions, the SimpleLinkedList of questions in the unit
+	 */
 	public SimpleLinkedList<Question> getQuestions() {
 		return questions;
-	}
+	} //End of getQuestions
 
+	/**
+	 * getIncorrectQuestions
+	 * Returns the incorrect questions answered in the unit
+	 * @return incorrectQuestions, the SimpleLinkedList of incorrect questions answered in the unit
+	 */
 	public SimpleLinkedList<Question> getIncorrectQuestion() {
 		return incorrectQuestions;
-	}
+	} //End of getIncorrectQuestions
 
+	/**
+	 * getQuestionsTaken
+	 * Returns the number of questions taken in the unit
+	 * @return questionsTaken, the number of questions taken in the unit
+	 */
 	public int getQuestionsTaken() {
 		return questionsTaken;
-	}
+	} //End of getQuestionsTaken
 
-	// private util method to generate a random int
-	//	private double randNum(int min, int range) {
-	//		return Math.random() * range + min;
-	//	}
-}
+} //End of class
